cf03adaa857348f952aa79e30ac9c7e06c0e31b8
athreyashettyb <shettyathreya@gmail.com>
2026-05-03 19:14:31 +0530

    746. Min Cost Climbing Stairs: Solution 2

--- topics/dynamic_programming/framework_for_dp/min_cost_climbing_stairs.py
+++ topics/dynamic_programming/framework_for_dp/min_cost_climbing_stairs.py
@@ -7,6 +7,7 @@ Time Complexity: O(n)
 
 Space Complexity: 
 """
+# Solution 1
 
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
@@ -21,4 +22,15 @@ class Solution:
             return memo[i]
 
         memo = {}
+        return dp(len(cost))
+    
+# Solution 2
+
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        @cache
+        def dp(i):
+            if i <= 1:
+                return 0      
+            return min(dp(i - 1) + cost[i - 1], dp(i - 2) + cost[i - 2])
         return dp(len(cost))
